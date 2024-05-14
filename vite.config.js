91593092d605c02f49723bda89bb5b57d616be1d
ramasakti <ramasakti1337@gmail.com>
2024-05-14 12:53:59 +0700

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/index-[hash].js',
+      },
+    },
+  },
+});
